32f193f26fe0487e508eb4a8683c69f25d402f4f
alexlyzhov <nikkou.postbox@gmail.com>
2021-03-22 17:35:20 +0000

    Separate cross-play eval into a script

--- marltoolbox/examples/tune_class_api/alternating_offers/train_cross_play.py
+++ marltoolbox/examples/tune_class_api/alternating_offers/train_cross_play.py
@@ -9,16 +9,6 @@ import copy
 from marltoolbox.algos.alternating_offers.alt_offers_training import AltOffersTraining
 from marltoolbox.utils import miscellaneous, log
 
-# import warnings
-# warnings.filterwarnings("ignore")
-
-# def warn(*args, **kwargs):
-#     pass
-# import warnings
-# warnings.warn = warn
-
-# ray.init(log_to_driver=False, configure_logging=True, logging_level=50)
-
 default_cond_params = {'utilities_skew': 0, 'prosociality_level': 0.3, 'hidden_embedding_size': 30, 'fairness_coeff': 0}
 
 def cond_params_iter():
@@ -28,43 +18,23 @@ def cond_params_iter():
         cur_params = copy.deepcopy(default_cond_params)
         cur_params['utilities_skew'] = utilities_skew
         yield cur_params
-#         yield {'utilities_skew': utilities_skew, 'prosociality_level': 0.3, 'hidden_embedding_size': 30, 'fairness_coeff': 0}
         
     for fairness_coeff in np.arange(0.0, 0.7, 0.1):
         cur_params = copy.deepcopy(default_cond_params)
         cur_params['fairness_coeff'] = fairness_coeff
         yield cur_params
-#         yield {'utilities_skew': 0, 'prosociality_level': 0.3, 'hidden_embedding_size': 30, 'fairness_coeff': fairness_coeff}
         
     for prosociality_level in np.arange(0, 1.1, 0.2):
         cur_params = copy.deepcopy(default_cond_params)
         cur_params['prosociality_level'] = prosociality_level
         yield cur_params
-#         yield {'utilities_skew': 0, 'prosociality_level': prosociality_level, 'hidden_embedding_size': 30, 'fairness_coeff': 0}
         
     for hidden_embedding_size in [5, 10, 20, 40, 80, 160]:
         cur_params = copy.deepcopy(default_cond_params)
         cur_params['hidden_embedding_size'] = hidden_embedding_size
         yield cur_params
-#         yield {'utilities_skew': 0, 'prosociality_level': 0.3, 'hidden_embedding_size': hidden_embedding_size, 'fairness_coeff': 0}
-
-def cond_params_iter_small():
-    yield default_cond_params  # default parameters
-    
-    for utilities_skew in np.arange(-1, 1.1, 0.4):
-        cur_params = copy.deepcopy(default_cond_params)
-        cur_params['utilities_skew'] = utilities_skew
-        yield cur_params
-#         yield {'utilities_skew': utilities_skew, 'prosociality_level': 0.3, 'hidden_embedding_size': 30, 'fairness_coeff': 0}
-        
-    for fairness_coeff in np.arange(0.0, 0.7, 0.1):
-        cur_params = copy.deepcopy(default_cond_params)
-        cur_params['fairness_coeff'] = fairness_coeff
-        yield cur_params
-#         yield {'utilities_skew': 0, 'prosociality_level': 0.3, 'hidden_embedding_size': 30, 'fairness_coeff': fairness_coeff}
 
 config = {
-    'name': 'cross_play_20K_episodes',
     'batch_size': 128,
     'training_episodes': 20000,
     
@@ -96,7 +66,13 @@ config = {
 #     'match_mode': tune.grid_search(['default_vs_custom']),
 #     'cond_params': tune.grid_search(list(cond_params_iter_small())),
     
-    'communication_mode': tune.grid_search(['cheap']),
+#     'name': 'cross_play_20K_episodes',
+#     'communication_mode': tune.grid_search(['cheap']),
+#     'match_mode': tune.grid_search(['custom_vs_custom_1', 'custom_vs_custom_2', 'default_vs_custom', 'custom_vs_default']),
+#     'cond_params': tune.grid_search(list(cond_params_iter())),
+    
+    'name': 'cross_play_20K_episodes_binding',
+    'communication_mode': tune.grid_search(['binding']),
     'match_mode': tune.grid_search(['custom_vs_custom_1', 'custom_vs_custom_2', 'default_vs_custom', 'custom_vs_default']),
     'cond_params': tune.grid_search(list(cond_params_iter())),
 }
@@ -137,23 +113,24 @@ class CrossPlayTraining(AltOffersTraining):
         
         super().setup(config)
         
-# each logical step of training contains several episodes, each episode is a batch of games
-training_steps = config['training_episodes'] // config['episodes_per_step']
-print(f'Num of training steps: {training_steps}')
-print(f'Episodes per step: {config["episodes_per_step"]}')
+if __name__ == '__main__':
+    # each logical step of training contains several episodes, each episode is a batch of games
+    training_steps = config['training_episodes'] // config['episodes_per_step']
+    print(f'Num of training steps: {training_steps}')
+    print(f'Episodes per step: {config["episodes_per_step"]}')
 
-exp_name_expanded, exp_dir = log.log_in_current_day_dir(config['name'])
+    exp_name_expanded, exp_dir = log.log_in_current_day_dir(config['name'])
 
-analysis = tune.run(
-    name=exp_name_expanded,
-    run_or_experiment=CrossPlayTraining,
-    stop={"training_iteration": training_steps},
-    config=config,
-    checkpoint_freq=1000,
-    checkpoint_at_end=True,
-    metric='prosocial_reward',
-    mode='max',
-)
+    analysis = tune.run(
+        name=exp_name_expanded,
+        run_or_experiment=CrossPlayTraining,
+        stop={"training_iteration": training_steps},
+        config=config,
+        checkpoint_freq=1000,
+        checkpoint_at_end=True,
+        metric='prosocial_reward',
+        mode='max',
+    )
 
-log.save_metrics(analysis, exp_name_expanded, "metrics.pickle")
-# log.pprint_saved_metrics(os.path.join(os.path.expanduser('~/ray_results'), exp_name_expanded, 'metrics.pickle'))
+    log.save_metrics(analysis, exp_name_expanded, "metrics.pickle")
+    # log.pprint_saved_metrics(os.path.join(os.path.expanduser('~/ray_results'), exp_name_expanded, 'metrics.pickle'))
